103d03687a0ad3ead04e3043fc35e1edf872b61f
aaqibjavaid <aaqibjavaid605@gmail.com>
2025-04-26 09:51:55 +0530

    feat: add merge script to combine all problems

---
+++ merge.js
@@ -0,0 +1,27 @@
+import fs from "fs";
+
+const lc = JSON.parse(
+  fs.readFileSync("./scrapper/leetcode/leetcode_problems.json", "utf8")
+);
+const cf = JSON.parse(
+  fs.readFileSync("./scrapper/codeforces/codeforces_problems.json", "utf8")
+);
+const cc = JSON.parse(
+  fs.readFileSync("./scrapper/codechef/codechef_problems.json", "utf8")
+);
+
+let idCounter = 1;
+const tag = (arr, name) =>
+  arr.map((p) => ({
+    id: idCounter++,
+    platform: name,
+    ...p,
+  }));
+
+const all = [
+  ...tag(lc, "LeetCode"),
+  ...tag(cf, "Codeforces"),
+  ...tag(cc, "CodeChef"),
+];
+
+fs.writeFileSync("all_problems.json", JSON.stringify(all, null, 2));
