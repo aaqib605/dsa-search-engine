f96e06e79e9ef3c0757d6154d2ccbaa6084d4415
aaqibjavaid <aaqibjavaid605@gmail.com>
2025-05-01 22:25:12 +0530

    feat: add auto-scrolling loop to collect up to TARGET problems

--- scrape.js
+++ scrape.js
@@ -1,33 +1,60 @@
 import puppeteer from "puppeteer";
+import fs from "fs";
 
-const scrapeLeetcode = async () => {
+const scrapeLeetcodeProblems = async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
   await page.goto("https://leetcode.com/problemset/");
 
+  const problemSelector =
+    "a.group.flex.flex-col.rounded-\\[8px\\].duration-300";
+
   await page.waitForSelector('a[id="1"]');
 
-  const problems = await page.evaluate(() => {
-    const sel = [
-      "a.group",
-      "flex",
-      "flex-col",
-      "rounded-\\[8px\\]",
-      "duration-300",
-    ].join(".");
+  let allProblems = [];
+  let prevCount = 0;
+  const TARGET = 300;
 
-    const nodes = Array.from(document.querySelectorAll(sel));
+  while (allProblems.length < TARGET) {
+    await page.evaluate((sel) => {
+      const currProblemsOnPage = document.querySelectorAll(sel);
 
-    return nodes.map((el) => ({
-      title: el.querySelector(".ellipsis.line-clamp-1")?.textContent.trim(),
-      url: el.href,
-    }));
-  });
+      if (currProblemsOnPage.length) {
+        currProblemsOnPage[currProblemsOnPage.length - 1].scrollIntoView({
+          behavior: "smooth",
+          block: "end",
+        });
+      }
+    }, problemSelector);
 
-  console.log("Result:", problems);
+    await page.waitForFunction(
+      (sel, prev) => document.querySelectorAll(sel).length > prev,
+      {},
+      problemSelector,
+      prevCount
+    );
+
+    allProblems = await page.evaluate((sel) => {
+      const nodes = Array.from(document.querySelectorAll(sel));
+
+      return nodes.map((el) => ({
+        title: el.querySelector(".ellipsis.line-clamp-1")?.textContent.trim(),
+        url: el.href,
+      }));
+    }, problemSelector);
+
+    prevCount = allProblems.length;
+  }
+
+  console.log(allProblems);
+
+  fs.writeFileSync(
+    "leetcode_problems.json",
+    JSON.stringify(allProblems, null, 2)
+  );
 
   await browser.close();
 };
 
-scrapeLeetcode();
+scrapeLeetcodeProblems();
