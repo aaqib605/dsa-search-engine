e11daad05c942a72954ecf1e31ce713bbfcfcff0
aaqibjavaid <aaqibjavaid605@gmail.com>
2025-05-02 10:00:00 +0530

    feat: enable headless mode by spoofing user-agent

--- scrape.js
+++ scrape.js
@@ -2,8 +2,15 @@ import puppeteer from "puppeteer";
 import fs from "fs";
 
 const scrapeLeetcodeProblems = async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
+
   const page = await browser.newPage();
+
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
+      "AppleWebKit/537.36 (KHTML, like Gecko) " +
+      "Chrome/114.0.5735.199 Safari/537.36"
+  );
 
   await page.goto("https://leetcode.com/problemset/");
 
